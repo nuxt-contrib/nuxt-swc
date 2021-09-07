f0ca21a44c9b5f397d64af2f790f00abed379c49
Pooya Parsa <pyapar@gmail.com>
2021-09-07 18:21:05 +0200

    refactor: use nuxt.options

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import defu from 'defu'
-import { ModuleThis } from '@nuxt/types/config/module'
 import { name, version } from '../package.json'
-import type { Options } from '@swc/core'
+import type { ModuleThis } from '@nuxt/types/config/module'
 import type { NuxtOptionsBuild } from '@nuxt/types/config/build'
+import type { Options } from '@swc/core'
 
 const defaultTargets = {
   server: { node: 12 },
@@ -14,29 +14,29 @@ const defaultTargets = {
 }
 
 function swcModule (this: ModuleThis) {
-  const { nuxt, options } = this
-  options.extensions.push('ts')
-  options.build.additionalExtensions = ['ts', 'tsx']
+  const { nuxt } = this
+  nuxt.options.extensions.push('ts')
+  nuxt.options.build.additionalExtensions = ['ts', 'tsx']
 
   // Auto detect corejs version:
   // https://github.com/nuxt/nuxt.js/tree/dev/packages/webpack/src/config/base.js#L124-L132
   let coreJs: string
-  if (options.build.corejs === 'auto') {
+  if (nuxt.options.build.corejs === 'auto') {
     try {
       coreJs = require('core-js/package.json').version.split('.')[0]
     } catch (_err) {
       coreJs = '2'
     }
   } else {
-    coreJs = String(options.build.corejs)
+    coreJs = String(nuxt.options.build.corejs)
   }
 
   nuxt.hook('webpack:config', (configs) => {
     for (const config of configs) {
-      const swcOptions: Options = defu(options.build.swc, {
+      const swcOptions: Options = defu(nuxt.options.build.swc, {
         // sync: true,
         sourceMaps: false,
-        minify: options.build.optimization?.minimize ?? (!options.dev && config.name !== 'server'),
+        minify: nuxt.options.build.optimization?.minimize ?? (!nuxt.options.dev && config.name !== 'server'),
         env: {
           coreJs,
           targets: defaultTargets[config.name]
